e80fa4e20ce5a97906bcc6988cc0d4a59011852a
Prasad Hp <prasadhp@outlook.in>
2024-07-14 12:58:49 +0530

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import mainRouter from "./routes/index";
 import dotenv from "dotenv"
+import cors from "cors"
 
 dotenv.config()
-
 const app = express();
 const port = process.env.PORT;
+app.use(cors())
 app.use(express.json())
 app.use("/identify", mainRouter)
 app.listen(port, ()=>console.log(`Listing at the Port ${port}`))
